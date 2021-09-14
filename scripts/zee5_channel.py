1d9e8a99d74c97811e32f71819c5a357f70459de
rds8896 <ramansingh274@gmail.com>
2021-09-15 01:08:55 +0530

    Update zee5_channel.py

--- scripts/zee5_channel.py
+++ scripts/zee5_channel.py
@@ -4,40 +4,40 @@ with open('./Zee_channel.m3u','w') as f:
 
     f.write('#EXTM3U url-tvg="http://botallen.live/epg.xml.gz"\n')
 
-    with open('Zee5_Entertainment.m3u') as file:
+    with open('./m3u/Zee5_Entertainment.m3u') as file:
         for line in file:
             if 'EXTM3U' in line:
                 continue
             else:
                 f.write(line)
 
-    with open('Zee5_news.m3u') as file:
+    with open('./m3u/Zee5_news.m3u') as file:
         for line in file:
             if 'EXTM3U' in line:
                 continue
             else:
                 f.write(line)
-    with open('Zee5_Lifestyle.m3u') as file:
-        for line in file:
-            if 'EXTM3U' in line:
-                continue
-            else:
-                f.write(line)
-
-    with open('Zee5_Movie.m3u') as file:
-        for line in file:
-            if 'EXTM3U' in line:
-                continue
-            else:
-                f.write(line)
-    with open('Zee5_Music.m3u') as file:
+    with open('./m3u/Zee5_Lifestyle.m3u') as file:
         for line in file:
             if 'EXTM3U' in line:
                 continue
             else:
                 f.write(line)
 
-    with open('mx_sony.m3u') as file:
+    with open('./m3u/Zee5_Movie.m3u') as file:
+        for line in file:
+            if 'EXTM3U' in line:
+                continue
+            else:
+                f.write(line)
+    with open('./m3u/Zee5_Music.m3u') as file:
+        for line in file:
+            if 'EXTM3U' in line:
+                continue
+            else:
+                f.write(line)
+
+    with open('./m3u/mx_sony.m3u') as file:
         for line in file:
             if 'EXTM3U' in line:
                 continue
